937b4cfa6a7a12e15cf8ce20624cf321ee6eb496
Omar Albarakeh <omaralbarakeh2@gmail.com>
2026-01-05 13:21:56 +0200

    auth module updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,26 +1,13 @@
 // src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { TokenService } from './token/token.service';
+import { TokenModule } from './token/token.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [
-    ConfigModule,
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        secret: config.getOrThrow<string>('JWT_ACCESS_SECRET'),
-        signOptions: {
-          expiresIn: '15m',
-        },
-      }),
-    }),
-  ],
+  imports: [TokenModule],
   controllers: [AuthController],
-  providers: [AuthService, TokenService, JwtStrategy],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
