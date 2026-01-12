5edc1ce3cdc4aa9bfb71580ad7a5d685c19708e5
Omar Albarakeh <omaralbarakeh2@gmail.com>
2026-01-12 14:57:18 +0200

    error responses updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-import { APP_GUARD, APP_FILTER } from '@nestjs/core';
+import { APP_GUARD } from '@nestjs/core';
 import * as Joi from 'joi';
 
 import { PrismaModule } from './prisma/prisma.module';
@@ -9,21 +9,17 @@ import { AuthModule } from './auth/auth.module';
 import { NewsletterModule } from './newsletter/newsletter.module';
 import { MessagesModule } from './message/messages.module';
 
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { RolesGuard } from './auth/guards/roles.guard';
-import { HttpErrorShapeFilter } from './common/filters/http-exception.filter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('development', 'test', 'production').required(),
-        PORT: Joi.number().default(3000),
-        DATABASE_URL: Joi.string().uri().required(),
         JWT_ACCESS_SECRET: Joi.string().min(32).required(),
         JWT_ACCESS_TTL: Joi.string().required(),
         JWT_REFRESH_TTL: Joi.string().required(),
-        CORS_ORIGIN: Joi.string().required(),
       }),
     }),
 
@@ -42,15 +38,18 @@ import { HttpErrorShapeFilter } from './common/filters/http-exception.filter';
     NewsletterModule,
     MessagesModule,
   ],
+
   providers: [
-    {
-      provide: APP_FILTER,
-      useClass: HttpErrorShapeFilter,
-    },
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
     },
+
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
